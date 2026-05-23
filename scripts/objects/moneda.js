353f491c4bf87d7ec0843250e48be72d3d981dcc
aike <aikesiero@gmail.com>
2026-05-23 15:01:46 +0200

    Monedas y contador

---
+++ scripts/objects/moneda.js
@@ -0,0 +1,20 @@
+export default class Moneda extends Phaser.Physics.Arcade.Sprite {
+
+    constructor(escena,x,y, iconoLlave, tipo){
+        super(escena, x, y, iconoLlave);
+        this.tipo = tipo;
+
+        this.escena = escena;
+        this.escena.add.existing(this);
+
+        // si es fisico añadir al mundo fisico
+        this.escena.physics.add.existing(this);
+
+        // desactivamos la gravedad para el objeto
+        this.body.allowGravity = false;
+    }
+
+    update(){
+
+    }
+}
